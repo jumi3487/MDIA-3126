5bb89b1f47eb2289146f3d479ef09bc76c3ce30d
Jumi Pham <npham32@my.bcit.ca>
2024-10-21 13:41:20 -0700

    organize folder

---
+++ mobileapp/README.md
@@ -0,0 +1,11 @@
+# 3 things you want to try during this course
+- learn how to make reponsive carousel
+- add to Google Map API
+- sound effect
+
+# 2 things you learned to today
+- link between pages
+- create new folders file
+
+# words for how you're feeling
+- great!
---
+++ mobileapp/app/(tab)/_layout.tsx
@@ -0,0 +1,31 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Tabs } from 'expo-router';
+
+
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="Sandwich"
+        options={{
+          title: '🥪',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
+        }}
+      />
+     <Tabs.Screen
+        name="Cheese"
+        options={{
+          title: '🧀',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+}
